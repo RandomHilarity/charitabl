b3e411a57bcb6ed013090758d4ce354e4ee0a1f7
John Webster <johnrwebs@gmail.com>
2019-09-30 14:54:26 -0600

    initial commit and setup

---
+++ src/components/landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Landing(props) {
+    return (
+        <section>
+            <span>
+                I'm a donor
+            </span>
+            <span>
+                I'm a charity
+            </span>
+        </section>
+    )
+}
