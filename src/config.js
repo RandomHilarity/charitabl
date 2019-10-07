c12a07f977f9d23e8f65722c29b56e3efd5e70fa
John Webster <johnrwebs@gmail.com>
2019-10-07 10:38:26 -0600

    local server to 8000

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
     API_PATH: {
-        development: "http://localhost:8080",
+        development: "http://localhost:8000",
         production: "https://charitabl-api.herokuapp.com"
     }[process.env.NODE_ENV || "development"]
 }
